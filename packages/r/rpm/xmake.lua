5f30eac28207550de0a8a9f741bbbb5b36f4ef63
ruki <waruqi@gmail.com>
2023-12-01 00:59:53 +0800

    add more deps

--- packages/r/rpm/xmake.lua
+++ packages/r/rpm/xmake.lua
@@ -9,7 +9,7 @@ package("rpm")
 
     add_deps("cmake", "lua")
     add_deps("python 3.x", {kind = "binary"})
-    add_deps("popt", "libcap", "acl", "sqlite3", "libarchive")
+    add_deps("popt", "libcap", "acl", "sqlite3", "libarchive", "elfutils", "lzma", "zstd", "zlib")
 
     on_install("linux", function (package)
         local configs = {
